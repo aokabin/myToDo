5a176196a613da0bb5536a05e7de12677ef9a126
Kodai Kinjo <info.aokabin@gmail.com>
2016-04-17 10:52:54 +0900

    Initial Commit

---
+++ myToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myToDo
+//
+//  Created by aokabin on 2016/04/17.
+//  Copyright © 2016年 aokabin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
